cff7823e7bb78c144e0262d90decebe1bede603c
geoffrey1330 <israelgeoffrey13@gmail.com>
2021-09-24 11:43:02 +0100

    adjust

--- django/core/settings.py
+++ django/core/settings.py
@@ -48,6 +48,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
--- django/core/urls.py
+++ django/core/urls.py
@@ -7,7 +7,7 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 from django.conf import settings
-from django.conf.urls.static import static
+from django.conf.urls.static import static   
 
 urlpatterns = [
     # API Token Management
